24c4a6d408b279705a39c456ca7c56679c638a44
shlok <ssagarwal@wpi.edu>
2021-05-05 09:27:43 -0400

    LeNet implemented. Car drives much better.

--- create_model.py
+++ create_model.py
@@ -24,12 +24,30 @@ X_train = np.array(images)
 y_train = np.array(measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
+from keras.layers import Flatten, Dense, Lambda, Convolution2D, MaxPool2D
 
 model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
-model.add(Flatten())
-model.add(Dense(1))
+
+def simple(model):
+    
+    model.add(Flatten())
+    model.add(Dense(1))
+    return model
+
+def LeNet(model):
+    model.add(Convolution2D(6,5,5,activation="relu"))
+    model.add(MaxPool2D())
+    model.add(Convolution2D(6,5,5,activation="relu"))
+    model.add(MaxPool2D())
+    model.add(Flatten())
+    model.add(Dense(120))
+    model.add(Dense(84))
+    model.add(Dense(1))
+    return model
+
+# model = simple(model)
+model = LeNet(model)
 
 model.compile(loss='mse', optimizer='adam')
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, epochs= 7)
