b348c28ad7f39b23494a98fdb44419fa30fa3f82
shlok <ssagarwal@wpi.edu>
2021-05-06 07:37:28 -0400

    better steering and does well on the track. the performance on the bridge and the area without side lines can be improved.

--- create_model.py
+++ create_model.py
@@ -11,14 +11,21 @@ with open('data/data/driving_log.csv') as csvfile:
 
 images =[]
 measurements = []
+correct_factor = 0.2
+num_cameras = 3
 for line in lines:
-    source_path = line[0]
-    filename = source_path.split('/')[-1]
-    current_path = 'data/data/IMG/' + filename
-    image = cv2.imread(current_path)
-    images.append(image)
-    measurement = float(line[3])
-    measurements.append(measurement)
+    for i in range(num_cameras):
+        source_path = line[i]
+        filename = source_path.split('/')[-1]
+        current_path = 'data/data/IMG/' + filename
+        image = cv2.imread(current_path)
+        images.append(image)
+        measurement = float(line[3])
+        if i == 1: # left camera, steer right
+            measurement += correct_factor
+        elif i == 2: # right camera, steer left
+            measurement -= correct_factor
+        measurements.append(measurement)
 
 augmented_images, augmented_measurements = [], []
 for image, measurement in zip(images, measurements):
@@ -31,9 +38,10 @@ X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Conv2D, MaxPool2D
+from keras.layers import Flatten, Dense, Lambda, Conv2D, MaxPool2D, Cropping2D
 
 model = Sequential()
+model.add(Cropping2D(cropping=((70,25), (0,0)), input_shape=(160,320,3)))
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, input_shape=(160,320,3)))
 
 def simple(model):
