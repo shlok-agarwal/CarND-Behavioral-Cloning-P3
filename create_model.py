e729ec76f053a1f22d4ddc69903de892a4404ba8
shlok <ssagarwal@wpi.edu>
2021-05-12 09:31:24 -0400

    Resnet 18 is training with small batch size and non augmented images. Need to get the generator working to use all the data

--- create_model.py
+++ create_model.py
@@ -7,7 +7,9 @@ from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Lambda, Cropping2D
 from models import simple, LeNet, GoogLeNet, Resnet_18, Resnet_50, Nvidia
-from preprocess_data import generator
+from preprocess_data import generator, getDataSet, getDataGen
+
+USE_RESNET_18 = True
 
 samples = []
 with open('data/data/driving_log.csv') as csvfile:
@@ -20,21 +22,31 @@ from sklearn.model_selection import train_test_split
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
 # Set our batch size
-batch_size=32
+BATCH_SIZE= 8 if USE_RESNET_18 else 32
 num_epochs = 5
 
 # compile and train the model using the generator function
-train_generator = generator(train_samples, batch_size=batch_size)
-validation_generator = generator(validation_samples, batch_size=batch_size)
+train_generator = generator(train_samples, batch_size=BATCH_SIZE)
+validation_generator = generator(validation_samples, batch_size=BATCH_SIZE)
 
-model = Nvidia()
+model = Resnet_18() if USE_RESNET_18 else Nvidia()
 print(model.summary())
 
 model.compile(loss='mse', optimizer='adam')
-history_object = model.fit_generator(train_generator, 
-            steps_per_epoch=np.ceil(len(train_samples)/batch_size), 
-            validation_data=validation_generator, 
-            validation_steps=np.ceil(len(validation_samples)/batch_size), 
+if USE_RESNET_18 == False:
+    history_object = model.fit_generator(train_generator, 
+                steps_per_epoch=np.ceil(len(train_samples)/BATCH_SIZE), 
+                validation_data=validation_generator, 
+                validation_steps=np.ceil(len(validation_samples)/BATCH_SIZE), 
+                epochs=num_epochs, verbose=1)
+else:
+    X_train, y_train = getDataSet(train_samples, augment = False, num_cameras = 1)
+    X_valid, y_valid = getDataSet(validation_samples, augment = False, num_cameras = 1)
+    gen = getDataGen()
+    history_object = model.fit_generator(gen.flow(X_train, y_train, batch_size=BATCH_SIZE), 
+            steps_per_epoch=np.ceil(len(train_samples)/BATCH_SIZE), 
+            validation_data=gen.flow(X_valid, y_valid, batch_size=BATCH_SIZE), 
+            validation_steps=np.ceil(len(validation_samples)/BATCH_SIZE), 
             epochs=num_epochs, verbose=1)
 
 model.save('model.h5')
--- preprocess_data.py
+++ preprocess_data.py
@@ -4,6 +4,7 @@ import sklearn
 import csv
 # # Shuffle data for randomness
 from sklearn.utils import shuffle
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 def generator(samples, batch_size=32, correct_factor = 0.2, num_cameras = 3):
     num_samples = len(samples)
@@ -43,11 +44,10 @@ def generator(samples, batch_size=32, correct_factor = 0.2, num_cameras = 3):
             y_train = np.array(augmented_measurements)
             yield sklearn.utils.shuffle(X_train, y_train)
 
-def getDataSet(samples):
+def getDataSet(samples, augment = True, num_cameras = 3):
     images =[]
     measurements = []
     correct_factor = 0.2
-    num_cameras = 3
     for line in samples:
         for i in range(num_cameras):
             source_path = line[i]
@@ -66,10 +66,59 @@ def getDataSet(samples):
     for image, measurement in zip(images, measurements):
         augmented_images.append(image)
         augmented_measurements.append(measurement)
-        augmented_images.append(cv2.flip(image, 1))
-        augmented_measurements.append(measurement*-1.0)
+        if augment:
+            augmented_images.append(cv2.flip(image, 1))
+            augmented_measurements.append(measurement*-1.0)
 
     # Neural networks
     X_train = np.array(augmented_images)
     y_train = np.array(augmented_measurements)
     return X_train, y_train
+
+def getDataGen():
+    # This will do preprocessing and realtime data augmentation:
+    datagen = ImageDataGenerator(
+        # set input mean to 0 over the dataset
+        featurewise_center=False,
+        # set each sample mean to 0
+        samplewise_center=False,
+        # divide inputs by std of dataset
+        featurewise_std_normalization=False,
+        # divide each input by its std
+        samplewise_std_normalization=False,
+        # apply ZCA whitening
+        zca_whitening=False,
+        # epsilon for ZCA whitening
+        zca_epsilon=1e-06,
+        # randomly rotate images in the range (deg 0 to 180)
+        rotation_range=0,
+        # randomly shift images horizontally
+        width_shift_range=0.1,
+        # randomly shift images vertically
+        height_shift_range=0.1,
+        # set range for random shear
+        shear_range=0.,
+        # set range for random zoom
+        zoom_range=0.,
+        # set range for random channel shifts
+        channel_shift_range=0.,
+        # set mode for filling points outside the input boundaries
+        fill_mode='nearest',
+        # value used for fill_mode = "constant"
+        cval=0.,
+        # randomly flip images
+        horizontal_flip=False,
+        # randomly flip images
+        vertical_flip=False,
+        # set rescaling factor (applied before any other transformation)
+        rescale=None,
+        # set function that will be applied on each input
+        preprocessing_function=None,
+        # image data format, either "channels_first" or "channels_last"
+        data_format=None,
+        # fraction of images reserved for validation 
+        # (strictly between 0 and 1)
+        validation_split=0.0)
+    
+    return datagen
+
